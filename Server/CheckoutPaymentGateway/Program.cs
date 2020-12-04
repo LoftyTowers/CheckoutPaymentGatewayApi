025d1eb8d727d2794b2ee3941f5cf7e59174256c
LoftyTowers <pgunary@gmail.com>
2020-12-04 14:03:19 +0000

    Added Kibana and elastic search

--- Server/CheckoutPaymentGateway/Program.cs
+++ Server/CheckoutPaymentGateway/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.IO;
-using CheckoutPaymentGateway;
 using Serilog;
 using System;
+using Serilog.Sinks.Elasticsearch;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using Serilog.Exceptions;
 
 namespace CheckoutPaymentGateway
 {
@@ -20,24 +22,50 @@ namespace CheckoutPaymentGateway
 		/// <param name="args"></param>
 		public static void Main(string[] args)
 		{
-			Log.Logger = new LoggerConfiguration()
-						 .Enrich.FromLogContext()
-						 .WriteTo.Console()
-						 .WriteTo.Elasticsearch()
-						 .MinimumLevel.Debug()
-						 .CreateLogger();
+			//configure logging first
+			ConfigureLogging();
 			try
 			{
 				CreateHostBuilder(args).Build().Run();
 			}
 			catch (Exception ex)
 			{
-				Log.Fatal(ex, "Application start-up failed");
+				Log.Fatal(ex, $"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}");
+				throw;
 			}
 			finally
 			{
 				Log.CloseAndFlush();
 			}
+		}
+		private static void ConfigureLogging()
+		{
+			var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile(
+					$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+					optional: true)
+				.Build();
+
+			Log.Logger = new LoggerConfiguration()
+				.Enrich.FromLogContext()
+				.Enrich.WithExceptionDetails()
+				.WriteTo.Debug()
+				.WriteTo.Console()
+				.WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+				.Enrich.WithProperty("Environment", environment)
+				.ReadFrom.Configuration(configuration)
+				.CreateLogger();
+		}
+
+		private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+		{
+			return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+			{
+				AutoRegisterTemplate = true,
+				IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+			};
 		}
 
 		/// <summary>
@@ -47,7 +75,13 @@ namespace CheckoutPaymentGateway
 		/// <returns>IHostBuilder</returns>
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 						Host.CreateDefaultBuilder(args)
-						.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+						.UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureAppConfiguration(configuration =>
+						{
+							configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+							configuration.AddJsonFile(
+								$"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+								optional: true);
+						})
 						.UseSerilog()
 						.ConfigureWebHostDefaults(webHostBuilder =>
 						{
